727e8713501a884c995bad532821a01fec4c82c6
faidz <faidzagustiawan@gmail.com>
2026-05-05 14:23:59 +0700

    faidz dorong

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: { "Content-Type": "application/json" },
 });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("spk_token");
